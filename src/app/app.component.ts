6b6b3d50fdf886cd039dcc8053ddf64377311f70
Dan Mace <dmacebeta@gmail.com>
2018-10-19 08:17:01 -0700

    create angular environment, load .json and test import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'mtg-test';
+  magic = require('./AllSets.json');
+  ngOnInit() {
+    console.log(this.magic);
+  }
 }
