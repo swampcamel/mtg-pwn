d474c32ad6d679ec8536782bab831e2410ca0efa
Student <example@example.com>
2018-10-23 13:31:12 -0700

    clear add values from form, add route to add component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CardListComponent } from './card-list/card-list.component';
+import { AddCardComponent } from './add-card/add-card.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: CardListComponent
+  },
+  {
+    path: 'add-card',
+    component: AddCardComponent
   }
 
 ];
