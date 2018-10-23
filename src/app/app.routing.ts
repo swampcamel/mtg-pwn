d20842c1903370e9ccaca928e5517eaa064fa6bc
Student <example@example.com>
2018-10-23 15:24:54 -0700

    more

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CardListComponent } from './card-list/card-list.component';
 import { AddCardComponent } from './add-card/add-card.component';
+import { DeckListComponent } from './deck-list/deck-list.component';
 
 const appRoutes: Routes = [
   {
@@ -11,8 +12,11 @@ const appRoutes: Routes = [
   {
     path: 'add-card',
     component: AddCardComponent
+  },
+  {
+    path: 'deck-list',
+    component: DeckListComponent
   }
-
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
